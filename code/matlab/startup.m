58120b856a9f83cb57fab86b9ff85aa9a6dcdb57
Ilse Dippenaar <ilsedipp@gmail.com>
2017-07-24 15:27:16 -0500

    [UNTESTED] Extending coherence calculations to MTM and variable length input

--- code/matlab/startup.m
+++ code/matlab/startup.m
@@ -1,7 +1,7 @@
 addpath external/FastICA_25
 addpath external/icasso122
 addpath external/export_fig
-addpath external/chronux_2_11
+addpath(genpath('external/chronux_2_11'))
 
 addpath plotting
 addpath analysis
